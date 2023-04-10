ba7d44dc0573b9eded5880c156c9fcef6f5bf618
man335h <manish.gehani@gmail.com>
2023-04-10 19:27:21 +0530

    added 2 new ML codes

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print('Adding all ML related code here')
+
+def model1():
+    print('Adding code for ML model 1')
+
+def model2():
+    print('Adding code for ML model 2')
+
+def model3():
+    print('Adding code for ML model 3')
